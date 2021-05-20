5e7fcb7ca4dd71e8045a8090c618f97ce19b682f
Zaid Saeed <68396459+zaidi7411@users.noreply.github.com>
2021-05-20 22:11:12 +0500

    Update changesellername.dart

--- clickandpick/lib/SellerDashboard/changesellername.dart
+++ clickandpick/lib/SellerDashboard/changesellername.dart
@@ -87,8 +87,8 @@ class _ChnageSellernameState extends State<ChnageSellername> {
             });
       },
       child: Container(
-        height: 80,
-        width: width / 1.5,
+        height: 60,
+        width: 180,
         decoration: BoxDecoration(
             gradient: mainButton,
             boxShadow: [
@@ -115,13 +115,13 @@ class _ChnageSellernameState extends State<ChnageSellername> {
       backgroundColor: Colors.grey[100],
       appBar: AppBar(
         iconTheme: IconThemeData(
-          color: Colors.black,
+          color: Colors.white,
         ),
         brightness: Brightness.light,
-        backgroundColor: Colors.transparent,
+        backgroundColor: Color(0xFFBB03B2),
         title: Text(
           'Settings',
-          style: TextStyle(color: darkGrey),
+          style: TextStyle(color: Colors.white),
         ),
         elevation: 0,
       ),
